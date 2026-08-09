eba121ece8a58615552cc069451b8b32e6953327
ty201457 <ty201457@gmail.com>
2026-08-09 18:02:05 +0800

    Replace isolated phonemes with syllable playback: lib/models/dictionary_entry.dart

--- lib/models/dictionary_entry.dart
+++ lib/models/dictionary_entry.dart
@@ -2,22 +2,26 @@ class DictionaryEntry {
   const DictionaryEntry({
     required this.word,
     required this.kkPhonetic,
-    required this.phonemes,
+    required this.syllables,
     this.translation,
   });
 
   final String word;
   final String kkPhonetic;
-  final List<PhonemeSegment> phonemes;
+  final List<SyllableSegment> syllables;
   final String? translation;
 }
 
-class PhonemeSegment {
-  const PhonemeSegment({
-    required this.symbol,
+class SyllableSegment {
+  const SyllableSegment({
+    required this.phonetic,
     required this.speechCue,
+    this.isPrimaryStress = false,
+    this.isSecondaryStress = false,
   });
 
-  final String symbol;
+  final String phonetic;
   final String speechCue;
+  final bool isPrimaryStress;
+  final bool isSecondaryStress;
 }
