fc0f89afa3e4a9db1e317491eff466bf45435137
ty201457 <ty201457@gmail.com>
2026-08-09 17:32:02 +0800

    Add KK phoneme playback: lib/models/dictionary_entry.dart

--- lib/models/dictionary_entry.dart
+++ lib/models/dictionary_entry.dart
@@ -1,9 +1,23 @@
 class DictionaryEntry {
   const DictionaryEntry({
     required this.word,
-    required this.phonetic,
+    required this.kkPhonetic,
+    required this.phonemes,
+    this.translation,
   });
 
   final String word;
-  final String phonetic;
+  final String kkPhonetic;
+  final List<PhonemeSegment> phonemes;
+  final String? translation;
+}
+
+class PhonemeSegment {
+  const PhonemeSegment({
+    required this.symbol,
+    required this.speechCue,
+  });
+
+  final String symbol;
+  final String speechCue;
 }
